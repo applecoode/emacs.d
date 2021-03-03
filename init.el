3399e28340f3970be163b72b0a7f402d176ce120
applecode <applecode88@gmail.com>
2021-03-03 21:06:43 +0800

    add begin hack

--- init.el
+++ init.el
@@ -3,3 +3,16 @@
 (package-initialize)
 (require 'evil)
 (evil-mode 1)
+
+(tool-bar-mode 1)
+(scroll-bar-mode 1)
+(electric-indent-mode 1)
+(setq inhibit-splash-screen t)
+
+(linum-mode t)
+
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "<f1>") 'open-my-init-file)
