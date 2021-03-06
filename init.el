02876d517c19a006a3769d05819b6885140deefd
xingyun0077 <xingyun0077@sina.com>
2021-03-06 10:13:37 +0800

    add some in office

--- init.el
+++ init.el
@@ -4,15 +4,44 @@
 (require 'evil)
 (evil-mode 1)
 
-(tool-bar-mode 1)
-(scroll-bar-mode 1)
-(electric-indent-mode 1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 (setq inhibit-splash-screen t)
 
-(linum-mode t)
+(global-linum-mode t)
 
+;;init file
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f2>") 'open-my-init-file)
 
-(global-set-key (kbd "<f1>") 'open-my-init-file)
+;;record hack
+(defun open-hack-file()
+  (interactive)
+  (find-file "C:/Users/Administrator/vimfiles/wiki/officewiki/org/emac_hack.org"))
+(global-set-key (kbd "<f1>") 'open-hack-file)
+
+;;recentf-mode
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(global-company-mode t)
+
+;;theme
+(load-theme 'deeper-blue)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company evil))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "微软雅黑" :foundry "outline" :slant normal :weight normal :height 143 :width normal)))))
