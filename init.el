0bf161376f3c3e80151bcea25907cf2fba4ff173
applecode <applecode88@gmail.com>
2021-03-06 23:34:40 +0800

    add org and some new file

--- init.el
+++ init.el
@@ -1,14 +1,65 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+			   ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+
+;;pkg
+(require 'cl)
+
+;; Add Packages
+(defvar my/packages '(
+		      ;; --- Auto-completion ---
+		      company
+		      ;; --- Better Editor ---
+		      hungry-delete
+		      swiper
+		      counsel
+		      smartparens
+		      ;; --- Major Mode ---
+		      js2-mode
+		      ;;vue-mode
+		      ;; --- Minor Mode ---
+		      nodejs-repl
+		      exec-path-from-shell
+		      ;; --- Themes ---
+		      monokai-theme
+		      ;; solarized-theme
+		      ) "Default packages")
+
+ (setq package-selected-packages my/packages)
+
+ (defun my/packages-installed-p ()
+     (loop for pkg in my/packages
+	   when (not (package-installed-p pkg)) do (return nil)
+	   finally (return t)))
+
+ (unless (my/packages-installed-p)
+     (message "%s" "Refreshing package database...")
+     (package-refresh-contents)
+     (dolist (pkg my/packages)
+       (when (not (package-installed-p pkg))
+	 (package-install pkg))))
+
+;;lovely vim
 (require 'evil)
 (evil-mode 1)
 
+;;org-mode config
+(require 'org)
+(setq org-src-fontify-natively t)
+(setq org-agenda-files '("~/org"))
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+;;make look comfortable
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq inhibit-splash-screen t)
-
+(setq inhibit-splash-screen t);cancel welcome information
 (global-linum-mode t)
+(toggle-truncate-lines 1) ;truncate line
+
+(add-to-list 'my/packages 'monokai-theme)
+(load-theme 'monokai 1)
 
 ;;init file
 (defun open-my-init-file()
@@ -19,7 +70,7 @@
 ;;record hack
 (defun open-hack-file()
   (interactive)
-  (find-file "C:/Users/Administrator/vimfiles/wiki/officewiki/org/emac_hack.org"))
+  (find-file "~/.emacs.d/emacs_hack.org"))
 (global-set-key (kbd "<f1>") 'open-hack-file)
 
 ;;recentf-mode
@@ -29,15 +80,35 @@
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 (global-company-mode t)
 
+;;search some help key
+(global-set-key "\C-h\ \C-f" 'find-function)
+(global-set-key "\C-h\ \C-v" 'find-variable)
+(global-set-key "\C-h\ \C-k" 'find-function-on-key)
+
+;;set cursor type
+(setq-default cursor-type 'bar)
+
+;;close backup-files
+(setq make-backup-file nil)
+
+;;select block edit
+(delete-selection-mode 1)
+
 ;;theme
 (load-theme 'deeper-blue)
+
+;;full screen
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;;highlight current line
+(global-hl-line-mode 1)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company evil))
+ '(package-selected-packages '(vue-mode company evil))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -45,3 +116,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "微软雅黑" :foundry "outline" :slant normal :weight normal :height 143 :width normal)))))
+
+
+;js-mode config
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
