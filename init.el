e54e0046308fd31621bee6cbbdd4ef982881b4e4
xingyun0077 <xingyun0077@sina.com>
2021-03-07 12:20:34 +0800

    add gtd

--- init.el
+++ init.el
@@ -81,7 +81,12 @@
 ;;company
 (global-company-mode t)
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(company-minimum-prefix-length 2)
+ '(org-agenda-files nil)
  '(package-selected-packages '(vue-mode company evil))
  '(tool-bar-mode nil))
 
@@ -111,6 +116,10 @@
 
 
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:family "微软雅黑" :foundry "outline" :slant normal :weight normal :height 143 :width normal)))))
 
 
